862b992b142b86398caf015b0ff4a25232a6fbcf
Tiago Almeida <tiago.almeida.poa@gmail.com>
2021-03-23 23:00:33 -0300

    testando classe de dominio - Cozinheiro

--- src/main/java/com/tiagoalmeida/comidaboa/resources/CozinheiroResource.java
+++ src/main/java/com/tiagoalmeida/comidaboa/resources/CozinheiroResource.java
@@ -1,16 +1,29 @@
 package com.tiagoalmeida.comidaboa.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tiagoalmeida.comidaboa.domain.Cozinheiro;
 
 @RestController
 @RequestMapping(value = "/cozinheiros")
 public class CozinheiroResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Cozinheiro> listar() {
+		
+		Cozinheiro coz1 = new Cozinheiro(1, "José", "85967485");
+		Cozinheiro coz2 = new Cozinheiro(2, "Pedro", "96857485");
+		
+		List<Cozinheiro> lista = new ArrayList<>();
+		lista.add(coz1);
+		lista.add(coz2);
+		
+		return lista;
 	}
 
 }
